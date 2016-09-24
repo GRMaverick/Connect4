48d4280b1044e9ba11a9abcc3e53077ef63ae492
GRMaverick <b020612e@student.staffs.ac.uk>
2016-09-24 16:51:55 +0200

    Tokens can be placed; Input Exception Handling

--- Connect-4/Board.py
+++ Connect-4/Board.py
@@ -1,19 +1,42 @@
 class Board(object):
-    __pBoard = 0
+    __pBoard = [0, 0, 0, 0, 0, 0 ,0, 0]
+    __pBoardGraphic = [0, 0, 0, 0, 0, 0, 0, 0]
     __pLastPlayer = 0
 
     def __init__(self):
-        __pBoard = [0, 0, 0, 0, 0, 0 ,0, 0]
-
-    def PlaceToken(position, player):
-        __pLastPlayer = player
-        # BASE-2 Calculation x**0
-        __pBoard[position] += __pBoard[position] * __pBoard[position] 
+        self.__pBoard = [0, 0, 0, 0, 0, 0 ,0, 0]
+        self.__pBoardGraphic = [0, 0, 0, 0, 0, 0, 0, 0]
+        self.__pLastPlayer = 0 
+    def Draw(self):
+        print()
+        for index in reversed(range(0, 8)):
+            print(self.__pBoard[0] & 2**index, " ", self.__pBoard[1] & 2**index, " ", self.__pBoard[2] & 2**index, " ", self.__pBoard[3] & 2**index, " ", self.__pBoard[4] &2**index, " ", self.__pBoard[5] & 2**index , " ", self.__pBoard[6] & 2**index, " ", self.__pBoard[7] & 2**index)
+        print()
         
-
-    def Draw():
-        print("Board Draw")
-        
-                
-
-
+    def GetNextAvailable(self, column):
+        BIT_POSITION = 0
+        BIT_SWITCH = 2 ** BIT_POSITION
+        while(self.__pBoard[column] & BIT_SWITCH):
+            BIT_POSITION += 1
+            BIT_SWITCH = 2 ** BIT_POSITION
+        return BIT_POSITION
+    def CheckColumnFull(self, column):
+        if(self.__pBoard[column-1] == 255):
+            return True
+        return False       
+    def PlaceToken(self, column, player):
+        if(column not in range(1,8)):
+            try: 
+                self.PlaceToken(int(input("Please enter a column between 1 and 8: ")), player) 
+            except ValueError:
+                print("Not a valid number")
+                self.PlaceToken(int(input("Please enter a column between 1 and 8: ")), player)
+        elif(self.CheckColumnFull(column) == True):
+            try:
+                self.PlaceToken(int(input("Column is full, please select another column: ")), player)
+            except ValueError:
+                print("Not a valid number")
+                self.PlaceToken(int(input("Please enter a column between 1 and 8: ")), player)
+        else:
+            self.__pLastPlayer = player
+            self.__pBoard[column-1] = self.__pBoard[column-1] + (2 ** self.GetNextAvailable(column-1))
--- Connect-4/Connect_4.py
+++ Connect-4/Connect_4.py
@@ -3,34 +3,36 @@ import os
 from Board import Board
 
 #GLOBALS
-GAME_LOOP_NOT_PAUSED=True
+GAME_LOOP=True
 PLAYER_TURN=0
 PLAYER_INPUT=0
-GAME_BOARD=Board
+GAME_BOARD=Board()
 
 def Initialise():  
-    GAME_LOOP_NOT_PAUSED = True
+    GAME_LOOP = True
     PLAYER_INPUT = 0
     Run()
 
 def Run():
-    while(GAME_LOOP_NOT_PAUSED):
-        Update()
+    while(GAME_LOOP):
+        CheckUserInput()
         Draw()
 
 def CheckUserInput():
-    PLAYER_INPUT = input("Enter a column number")
-    os.system('cls')
-    print("Player:", PLAYER_TURN+1, "has entered:", PLAYER_INPUT)
-
-def Update():
-    CheckUserInput()
+    try:
+        PLAYER_INPUT = int(input("Enter a column number: "))
+    except ValueError:
+        print("Not a valid number")
+        CheckUserInput()
+    else:
+        os.system('cls')
+        print("Player:", PLAYER_TURN+1, "has entered:", PLAYER_INPUT)
+        GAME_BOARD.PlaceToken(PLAYER_INPUT, PLAYER_TURN)
 
 def Draw():
     global PLAYER_TURN
     PLAYER_TURN = (PLAYER_TURN + 1) % 2
     GAME_BOARD.Draw()
     print("Player: ", PLAYER_TURN+1)   
-
 
 Initialise()
