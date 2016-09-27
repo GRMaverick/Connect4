02942e57878cc08c072aa7c67a59f4bf16fc79e4
GRMaverick <b020612e@student.staffs.ac.uk>
2016-09-28 00:41:21 +0200

    Connect_4 Game Complete, Requires Testing

--- Connect-4/Board.py
+++ Connect-4/Board.py
@@ -1,29 +1,78 @@
 class Board(object):
-    __pBoard = [0, 0, 0, 0, 0, 0 ,0, 0]
-    __pBoardGraphic = [0, 0, 0, 0, 0, 0, 0, 0]
+    __pHeight = 6
+    __pWidth = 7
+    __pBoard = [0, 0, 0, 0, 0, 0 ,0]
+    __pBoardGraphic = [0, 0, 0, 0, 0, 0, 0]
     __pLastPlayer = 0
 
+    def CheckHorizontal(self):
+        for i in range(0, 4):
+            for x in reversed(range(0,self.__pHeight)):
+                check1 = ((self.__pBoard[i] & 2**x) and (self.__pBoard[i+1] & 2**x) and (self.__pBoard[i+2] & 2**x) and (self.__pBoard[i+3] & 2**x))    
+                check2 = ((self.__pBoardGraphic[i] & 2**x == 0) and (self.__pBoardGraphic[i+1] & 2**x == 0) and (self.__pBoardGraphic[i+2] & 2**x == 0) and (self.__pBoardGraphic[i+3] & 2**x == 0))
+                check3 = ((self.__pBoardGraphic[i] & 2**x) and (self.__pBoardGraphic[i+1] & 2**x) and (self.__pBoardGraphic[i+2] & 2**x) and (self.__pBoardGraphic[i+3] & 2**x))
+                if(check1 and (check2 or check3)):
+                    return self.__pLastPlayer
+        return -1
+
+    def CheckVertical(self):
+            for x in range(0, self.__pWidth):
+                for i in range(0,3):
+                    check1 = (self.__pBoard[x] & 2**i) and (self.__pBoard[x] & 2**(i+1)) and (self.__pBoard[x] & 2**(i+2)) and (self.__pBoard[x] & 2**(i+3))
+                    check2 = (self.__pBoardGraphic[x] & 2**i == 0) and (self.__pBoardGraphic[x] & 2**(i+1) == 0) and (self.__pBoardGraphic[x] & 2**(i+2) == 0) and (self.__pBoardGraphic[x] & 2**(i+3) == 0)
+                    check3 = (self.__pBoardGraphic[x] & 2**i) and (self.__pBoardGraphic[x] & 2**(i+1)) and (self.__pBoardGraphic[x] & 2**(i+2)) and (self.__pBoardGraphic[x] & 2**(i+3))
+                    if(check1 and (check2 or check3)):
+                        return self.__pLastPlayer
+            return -1
+
+    def CheckDiagonal(self):
+        for n in reversed(range(3,6)):
+            for i in range(0, 4):
+                check1 = (self.__pBoard[i] & 2**n) and (self.__pBoard[i+1] & 2**(n-1)) and (self.__pBoard[i+2] & 2**(n-2)) and (self.__pBoard[i+3] & 2**(n-3))
+                check2 = (self.__pBoardGraphic[i] & 2**n == 0) and (self.__pBoardGraphic[i+1] & 2**(n-1) == 0) and (self.__pBoardGraphic[i+2] & 2**(n-2) == 0) and (self.__pBoardGraphic[i+3] & 2**(n-3) == 0)
+                check3 = (self.__pBoardGraphic[i] & 2**n) and (self.__pBoardGraphic[i+1] & 2**(n-1)) and (self.__pBoardGraphic[i+2] & 2**(n-2)) and (self.__pBoardGraphic[i+3] & 2**(n-3))
+                if(check1 and (check2 or check3)):
+                    return self.__pLastPlayer
+        for n in reversed(range(3, 6)):
+            for i in reversed(range(3, 7)):
+                check1 = (self.__pBoard[i] & 2**n) and (self.__pBoard[i-1] & 2**(n-1)) and (self.__pBoard[i-2] & 2**(n-2)) and (self.__pBoard[i-3] & 2**(n-3))
+                check2 = (self.__pBoardGraphic[i] & 2**n == 0) and (self.__pBoardGraphic[i-1] & 2**(n-1) == 0) and (self.__pBoardGraphic[i-2] & 2**(n-2) == 0) and (self.__pBoardGraphic[i-3] & 2**(n-3) == 0)
+                check3 = (self.__pBoardGraphic[i] & 2**n) and (self.__pBoardGraphic[i-1] & 2**(n-1)) and (self.__pBoardGraphic[i-2] & 2**(n-2)) and (self.__pBoardGraphic[i-3] & 2**(n-3))
+                if(check1 and (check2 or check3)):
+                    return self.__pLastPlayer
+        return -1
+
     def __init__(self):
-        self.__pBoard = [0, 0, 0, 0, 0, 0 ,0, 0]
-        self.__pBoardGraphic = [0, 0, 0, 0, 0, 0, 0, 0]
+        self.__pBoard = [0, 0, 0, 0, 0, 0 ,0]
+        self.__pBoardGraphic = [0, 0, 0, 0, 0, 0 ,0]
         self.__pLastPlayer = 0 
+
+    def Initialise(self):
+        self.__pBoard = [0, 0, 0, 0, 0, 0 ,0]
+        self.__pBoardGraphic = [0, 0, 0, 0, 0, 0 ,0]
+        self.__pLastPlayer = 0 
+
+    def CheckWinner(self):
+        if(self.CheckHorizontal() != -1):
+            return self.__pLastPlayer
+        if(self.CheckVertical() != -1):
+            return self.__pLastPlayer
+        if(self.CheckDiagonal() != -1):
+            return self.__pLastPlayer
+        return -1
+
     def Draw(self):
         print()
-        for index in reversed(range(0, 8)):
-            print(self.__pBoardGraphic[0] & 2**index, " ", self.__pBoardGraphic[1] & 2**index, " ", self.__pBoardGraphic[2] & 2**index, " ", self.__pBoardGraphic[3] & 2**index, " ", self.__pBoardGraphic[4] &2**index, " ", self.__pBoardGraphic[5] & 2**index , " ", self.__pBoardGraphic[6] & 2**index, " ", self.__pBoardGraphic[7] & 2**index)
+        for index in reversed(range(0, self.__pHeight)):
+            print(
+                "[        ]" if self.__pBoard[0] & 2**index == 0 else "[   RED  ]" if self.__pBoardGraphic[0] & 2**index else "[ YELLOW ]" if self.__pBoardGraphic[0] & 2**index == 0 else print("X"),
+                "[        ]" if self.__pBoard[1] & 2**index == 0 else "[   RED  ]" if self.__pBoardGraphic[1] & 2**index else "[ YELLOW ]" if self.__pBoardGraphic[1] & 2**index == 0 else print("X"),
+                "[        ]" if self.__pBoard[2] & 2**index == 0 else "[   RED  ]" if self.__pBoardGraphic[2] & 2**index else "[ YELLOW ]" if self.__pBoardGraphic[2] & 2**index == 0 else print("X"),
+                "[        ]" if self.__pBoard[3] & 2**index == 0 else "[   RED  ]" if self.__pBoardGraphic[3] & 2**index else "[ YELLOW ]" if self.__pBoardGraphic[3] & 2**index == 0  else print("X"),
+                "[        ]" if self.__pBoard[4] & 2**index == 0 else "[   RED  ]" if self.__pBoardGraphic[4] & 2**index else "[ YELLOW ]" if self.__pBoardGraphic[4] & 2**index == 0  else print("X"),
+                "[        ]" if self.__pBoard[5] & 2**index == 0 else "[   RED  ]" if self.__pBoardGraphic[5] & 2**index else "[ YELLOW ]" if self.__pBoardGraphic[5] & 2**index == 0  else print("X"),
+                "[        ]" if self.__pBoard[6] & 2**index == 0 else "[   RED  ]" if self.__pBoardGraphic[6] & 2**index else "[ YELLOW ]" if self.__pBoardGraphic[6] & 2**index == 0  else print("X"))
         print()
-
-        # DEBUG
-        for index in reversed(range(0, 8)):
-            print(self.__pBoard[0] & 2**index, " ", self.__pBoard[1] & 2**index, " ", self.__pBoard[2] & 2**index, " ", self.__pBoard[3] & 2**index, " ", self.__pBoard[4] &2**index, " ", self.__pBoard[5] & 2**index , " ", self.__pBoard[6] & 2**index, " ", self.__pBoard[7] & 2**index)
-        print()
-
-# for each binary position
-        # arrayposition * 2**BinaryPosition
-        # if ^ = 0
-            # YELLOW
-        #else
-            # RED
         
     def GetNextAvailable(self, board, column):
         BIT_POSITION = 0
@@ -32,12 +81,12 @@ class Board(object):
             BIT_POSITION += 1
             BIT_SWITCH = 2 ** BIT_POSITION
         return BIT_POSITION
+
     def CheckColumnFull(self, column):
-        if(self.__pBoard[column-1] == 255):
-            return True
-        return False       
+        return (self.__pBoard[column-1] == 2 ** self.__pHeight-1)
+
     def PlaceToken(self, column, player):
-        if(column not in range(1,8)):
+        if(column not in range(1, self.__pWidth + 1)):
             try: 
                 self.PlaceToken(int(input("Please enter a column between 1 and 8: ")), player) 
             except ValueError:
@@ -54,5 +103,4 @@ class Board(object):
             value = (2 ** self.GetNextAvailable(self.__pBoard, column-1))
             self.__pBoard[column-1] = self.__pBoard[column-1] + value
             if(player == 0):
-               self.__pBoardGraphic[column-1] = self.__pBoardGraphic[column-1] + value
-            
+                self.__pBoardGraphic[column-1] = self.__pBoardGraphic[column-1] + value
--- Connect-4/Connect_4.py
+++ Connect-4/Connect_4.py
@@ -3,20 +3,29 @@ import os
 from Board import Board
 
 #GLOBALS
-GAME_LOOP=True
 PLAYER_TURN=0
 PLAYER_INPUT=0
 GAME_BOARD=Board()
+WINNER=-1
 
 def Initialise():  
-    GAME_LOOP = True
+    global WINNER
+    global PLAYER_TURN
+    global PLAYER_INPUT
+    WINNER = -1
+    PLAYER_TURN = 0
     PLAYER_INPUT = 0
+    GAME_BOARD.Initialise()
     Run()
 
 def Run():
-    while(GAME_LOOP):
+    global WINNER
+    while(WINNER == -1):
         CheckUserInput()
+        WINNER = GAME_BOARD.CheckWinner()
         Draw()
+
+    GameOver()
 
 def CheckUserInput():
     try:
@@ -33,6 +42,22 @@ def Draw():
     global PLAYER_TURN
     PLAYER_TURN = (PLAYER_TURN + 1) % 2
     GAME_BOARD.Draw()
-    print("Player: ", PLAYER_TURN+1)   
+    print("Player: ", PLAYER_TURN+1)
+
+def GameOver():
+    print("The winner is:", WINNER)
+    try:
+        PLAYER_INPUT = input("Play again? ( Yes | No )")
+    except ValueError:
+        print("Input not valid")
+        GameOver()
+    else:
+        if(PLAYER_INPUT == "y"):
+            Initialise()
+        elif(PLAYER_INPUT == "n"):
+            os.close(0)
+        else:
+            print("Input not valid")
+            GameOver()
 
 Initialise()
